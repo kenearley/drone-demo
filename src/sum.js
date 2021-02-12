5bb78c216e8598599564116cb937919e27069b83
Ken Earley <ken@mjg.in>
2021-02-12 00:15:01 -0600

    Make slowSum slighty less slow

--- src/sum.js
+++ src/sum.js
@@ -4,7 +4,7 @@ export function sum(a, b) {
 
 export function slowSum(a, b) {
   let foo = 1;
-  for (let i = 0; i < 10000000000; i++) {
+  for (let i = 0; i < 1000000000; i++) {
     foo++;
   }
   return sum(a, b) + foo - foo;
