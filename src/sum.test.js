01dc1de75ecdc9d32fc7189b382ab2dc3785e405
Ken Earley <ken@mjg.in>
2021-02-11 22:41:38 -0600

    Move slow test into its own pipeline

--- src/sum.test.js
+++ src/sum.test.js
@@ -3,3 +3,17 @@ import { sum } from "./sum";
 test("adds 1 + 2 to equal 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test("adds 1 + -1 to equal 0", () => {
+  expect(sum(1, -1)).toBe(0);
+});
+
+test("adds 1 + -2 to equal -1", () => {
+  expect(sum(1, -2)).toBe(-1);
+});
+
+describe("slow test @slow", () => {
+  test("adds 5 + 4 to equal 9 (slowly)", () => {
+    setTimeout(() => expect(sum(5, 4).toBe(9)), 10000);
+  });
+});
